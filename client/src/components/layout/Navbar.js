1638be8714444ed58068da15a1f77907551e7f20
Rob <robdawg@gmail.com>
2018-08-19 22:52:43 -0400

    removed nav bar links for now

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -34,9 +34,9 @@ class Navbar extends Component {
 
     const guestLinks = (
       <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <Link className="nav-link" to="/register">Sign In</Link>
-        </li>
+        </li> */}
       </ul>
     );
 
@@ -52,8 +52,8 @@ class Navbar extends Component {
           <div className="collapse navbar-collapse" id="mobile-nav">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item">
-                <Link className="nav-link" to="/lists"> Lists
-                </Link>
+                {/* <Link className="nav-link" to="/lists"> Lists
+                </Link> */}
               </li>
             </ul>
             {isAuthenticated ? authLinks : guestLinks }
