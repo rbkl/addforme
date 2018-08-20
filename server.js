641ca750f187aecbfe3c806434c17a147ee13dd2
Rob <robdawg@gmail.com>
2018-08-19 22:31:12 -0400

    first production build

--- server.js
+++ server.js
@@ -51,4 +51,4 @@ if(process.env.NODE_ENV === 'production') {
 
 const port = process.env.PORT || 5000;
 
-var server = app.listen(port, () => console.log(`Server running on port: ${port}`));
+app.listen(port, () => console.log(`Server running on port: ${port}`));
